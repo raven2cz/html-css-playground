868800a4e6d8b07c75496064d7d7a2925dc4f82c
Antonin Fischer <tonda.fischer@gmail.com>
2022-09-20 22:25:47 +0200

    Add next subprojects for dark modes.

---
+++ light-dark-modes-5/script/darkmode.js
@@ -0,0 +1,5 @@
+document.getElementById('toggle').addEventListener('click', () => {
+    document.getElementById('blender').classList.toggle('expand');
+    document.getElementById('toggle').classList.toggle('s-module--hasZIndex--1QkpC');
+});
+  
